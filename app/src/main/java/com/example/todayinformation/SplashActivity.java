280983be11b77bbf9c5684dc249e139f58becdca
BasicT <qibuchads@163.com>
2019-06-12 11:44:46 +0800

    splash页面设计

---
+++ app/src/main/java/com/example/todayinformation/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.todayinformation;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private View mFullScreenVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mFullScreenVideoView = (FullScreenVideoView)findViewById(R.id.fsvv_play);
+        ((FullScreenVideoView) mFullScreenVideoView).setVideoURI(Uri.parse("android.resource://" + getPackageName() +
+                File.separator + R.raw.splash));
+        ((FullScreenVideoView) mFullScreenVideoView).setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        ((FullScreenVideoView) mFullScreenVideoView).setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+    }
+}
